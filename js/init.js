b135b02268f920744a4854452f4bc0bd04f22a67
Luqman Yasin <luqman.yasin@gmail.com>
2016-03-18 13:59:01 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -22,3 +22,17 @@ function getComb(array) {
   }
   return(res);
 }
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
