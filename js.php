139c68af94b8b8155b852612e04e3e4c5fdaeadd
Luqman Yasin <luqman.yasin@gmail.com>
2016-03-18 13:58:18 +0000

    Work in progress JavaScript implementation.

--- js.php
+++ js.php
@@ -1,9 +1,29 @@
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-  <script src="js/init.js"></script>
-  <script>
-    $(document).ready(function(){
-      var arrys = getComb('1,2,3,4,5,6,7');
-      console.log(arrys);
+<div class="forms"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+<script src="js/init.js"></script>
+<script>
+  var successArr 	= ['success', 'logged in', 'successful', 'welcome', 'hello', 'successfully', 'redirecting', 'please wait']; // Array of keywords that could be interpreted as successful
+  var method = $_GET('m');
+  var action = $_GET('a');
+  var named = $_GET('n');
+  var name = $_GET('n');
+</script>
+<script src="js/submit.js"></script>
+<script>
+  $(document).ready(function(){
+    var values = $_GET('values');
+    var values = values.replace(/\%2C/g, ',');
+    console.log(values);
+    var arrys = getComb(values);
+    console.log(arrys);
+    var cntn = parseInt(1);
+    $(arrys).each(function(){
+      var input = this;
+      var formd = "<form style='margin:50px auto'><input type='text' value='"+input+"' class='value' name='"+name+"' disabled/><input type='submit' value='Submit' class='sub' /><input type='hidden' value='"+cntn+"' /><br /> <span class='stuff'>Not Submitted</span></form>";
+      $('.forms').append(formd);
+      var cntn = parseInt(cntn)+1;
+      console.log(cntn);
     });
-  </script>
-  <!-- Runs the new jQuery powered getComb, this version actually loads :D -->
+  });
+
+</script>
