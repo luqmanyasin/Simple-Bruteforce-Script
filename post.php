3739cad4110759bc2a6a9d03c227999e2c8e7ab9
Luqman Yasin <luqman.yasin@gmail.com>
2016-03-22 09:44:15 +0000

    Update post.php

--- post.php
+++ post.php
@@ -1,9 +1,13 @@
 <?
 if($_POST['code']):
   if($_POST['code'] == 'abcd'):
+    print 'Welcome '.$_POST['user'];
     exit;
-  elseif($_POST['code'] == 'dcab'):
-    print 'Logged In';
+  elseif($_POST['code'] == 'dcab' && $_POST['user'] == 'exampleUser' || $_POST['code'] == 'test1' && $_POST['user'] == 'exampleUser'):
+    print '<h1>Logged In</h1>';
+    print 'Welcome '.$_POST['user'];
+    exit;
+  elseif($_POST['code'] == 'cabd'):
     exit;
   elseif($_POST['code'] == 'cdab'):
     print 'Successfully logged in';
